2d939bc83b28b1ecb93efb87a0acd0f66e46eb77
Cameron Kinnear <cmkin402@gmail.com>
2026-09-17 10:11:04 -0400

    XMLRequest and header added

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,5 @@
+let url = window.location.href;
+
 userId = "";
 firstName = "";
 lastName = "";
@@ -11,13 +13,17 @@ function AttemptLogin() {
     let password = loginPassword.value.trim();
     
     let jsonPayload = JSON.stringify({username: username, password: password});
-
+    
+    // Set up HTTPS request
+    let xhr = XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
     try {
     /*
         User API to request and parse with the gathered info
     */
         console.log(jsonPayload);
-       window.location.href = 'contacts.html';
+        window.location.href = 'contacts.html';
 
     }
     catch (err) {
