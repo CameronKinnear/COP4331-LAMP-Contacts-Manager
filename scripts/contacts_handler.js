531eef908287dae706649c72aa7cbd6f62478fac
plaserd <cmkin402@gmail.com>
2026-09-24 11:53:14 -0400

    name selection and widgets popup

--- scripts/contacts_handler.js
+++ scripts/contacts_handler.js
@@ -4,14 +4,18 @@ function AddContact() {
 }
 
 let contactImage = document.getElementById('selected-contact-img');
+let contactfName = document.getElementById('selected-contact-first-name');
+let contactlName = document.getElementById('selected-contact-last-name');
+
 function DisplaySelectedContact(parent) {
     const image = parent.querySelector('.contact-icon');
     const firstName = parent.querySelector('.contact-first-name');
     const lastName = parent.querySelector('.contact-last-name');
     const phone = parent.querySelector('.contact-phone');
 
-    console.log(image);
     contactImage.src = image.src;
+    contactfName.innerHTML = firstName.innerHTML
+    contactlName.innerHTML = lastName.innerHTML
 }
 
 function GetChildren(self) {
